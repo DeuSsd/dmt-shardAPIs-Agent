dcc22f5c4edbcfcbc0953f0e835ae169faef2753
IlyaG01 <golovakov.ilya@gmail.com>
2022-05-19 22:49:31 +0300

    made connection with DB and sending JSON with everything APIs

--- src/agentAPI/backed_api/views.py
+++ src/agentAPI/backed_api/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import APIData
 from .main import one_task
-
+import connect_with_sql
 from .serializer import APISerializer
 
 class RESTAPIView(APIView):
@@ -13,16 +13,25 @@ class RESTAPIView(APIView):
         print(request.data)
         if request.data == {}:
             return Response({'title': 'GO OUT!!!'})  # для вызова из браузера
-        list_res=[]
-        task_id = request.data['task_id']
-        user_id = request.data['user_id']
-        tasks = request.data['tasks']
-        k=0
-        while k<len(tasks):
-            res=one_task(tasks[k])
-            list_res.append(res)
-            k=k+1
-        return Response({'task_id':task_id,'user_id': user_id, 'tasks':list_res})
+        if request.data['request']=='get_api':
+            list_api_param=[]
+            apis=connect_with_sql.get_API()
+            for i in range(len(apis)):
+                res=connect_with_sql.get_param(i+1)
+                api={'api': apis[i],'parameters':res}
+                list_api_param.append(api)
+            return Response(list_api_param)
+        if request.data['request']=='get_data':
+            list_res=[]
+            task_id = request.data['task_id']
+            user_id = request.data['user_id']
+            tasks = request.data['tasks']
+            k=0
+            while k<len(tasks):
+                res=one_task(tasks[k])
+                list_res.append(res)
+                k=k+1
+            return Response({'task_id':task_id,'user_id': user_id, 'tasks':list_res})
 
 
     def post(self, request):
---
+++ src/agentAPI/connect_with_sql.py
@@ -0,0 +1,48 @@
+import sqlite3
+conn = sqlite3.connect('web_api.db')
+cur = conn.cursor()
+import json
+
+#cur.execute("""CREATE TABLE IF NOT EXISTS web_api(api_id INT PRIMARY KEY,name_api TEXT,website TEXT);""")
+#conn.commit()
+
+#cur.execute("""CREATE TABLE IF NOT EXISTS param_api(id INT PRIMARY KEY,api_id INT,parameters_api TEXT,type_parameters TEXT);""")
+#conn.commit()
+
+#api1 = ('2', 'COVIDAPI', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+#api_par_1=('4','2','start_time','time')
+#api_par_2=('5','2','end_time','time')
+#api_par_3=('6','2','location','string')
+#cur.execute("INSERT INTO web_api VALUES(?, ?, ?);", api1)
+#cur.execute("INSERT INTO param_api VALUES(?, ?, ?, ?);", api_par_1)
+#cur.execute("INSERT INTO param_api VALUES(?, ?, ?, ?);", api_par_2)
+#cur.execute("INSERT INTO param_api VALUES(?, ?, ?, ?);", api_par_3)
+#conn.commit()
+
+def get_API():
+    conn = sqlite3.connect('web_api.db')
+    cur = conn.cursor()
+    list_api=[]
+    cur.execute("SELECT name_api FROM web_api;")
+    res=cur.fetchall()
+    for i in range(len(res)):
+        list_api.append(res[i][0])
+    return list_api
+
+#print(get_API())
+
+
+def get_param(id_api):
+    conn = sqlite3.connect('web_api.db')
+    cur = conn.cursor()
+    cur.execute("""SELECT parameters_api, type_parameters FROM param_api
+        WHERE api_id=?;""", (str(id_api)))
+    res = cur.fetchall()
+    list_param=[]
+    for i in range(len(res)):
+        list_param.append({'param': res[i][0], 'type':res[i][1]})
+        #print(res[i][0]+": "+res[i][1])
+
+    return list_param
+a=get_param(2)
+print(a)
