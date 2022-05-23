7efbca00d53d2276cf351b15274452aa85fc4f41
IlyaG01 <golovakov.ilya@gmail.com>
2022-05-23 18:36:11 +0300

    made insides

--- src/agentAPI/backed_api/views.py
+++ src/agentAPI/backed_api/views.py
@@ -25,7 +25,7 @@ class RESTAPIView2(APIView):
     def post(self, request):
         task_id = request.data['task_id']
         user_id = request.data['user_id']
-        some_api = request.data['APIs']
+        some_api = request.data['insides']
         list_api_param = []
         for i in range(len(some_api)):
             param = connect_with_sql.get_param(i + 1)
@@ -34,18 +34,18 @@ class RESTAPIView2(APIView):
             api = {'api': api, 'parameters': param}
             # parameter={'parameters':param}
             list_api_param.append(api)
-        return Response({'task_id': task_id, 'user_id': user_id, 'tasks': list_api_param})
+        return Response({'task_id': task_id, 'user_id': user_id, 'insides': list_api_param})
 
 class RESTAPIView3(APIView):
     def post(self, request):
         list_res = []
         task_id = request.data['task_id']
         user_id = request.data['user_id']
-        tasks = request.data['tasks']
+        tasks = request.data['insides']
         for k in range(len(tasks)):
             res = get_web_from_name(tasks[k])  # получил ссылку на отдельный АПИ
             request_to_anton = one_task(res)  # это я отправляю Антону JSON с ссылкой и параметрами и получаю от него ответ
             web_api = connect_with_sql.from_web_to_api(request_to_anton['web'])  # заменяю в ответе Антона ссылку на название АПИ
             request_to_user = {'api': web_api,'data': [request_to_anton['parameters']]}  # формирую ответ из названия АПИ и его данных
             list_res.append(request_to_user)
-        return Response({'task_id': task_id, 'user_id': user_id, 'task_data': list_res})
+        return Response({'task_id': task_id, 'user_id': user_id, 'insides': list_res})
