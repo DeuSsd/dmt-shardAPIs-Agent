6831bcead96235fbaa80e2319175cd649eb5351e
IlyaG01 <golovakov.ilya@gmail.com>
2022-05-23 18:32:12 +0300

    made urls

--- src/agentAPI/agentAPI/urls.py
+++ src/agentAPI/agentAPI/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from backed_api.views import RESTAPIView
+from django.urls import path, include
+from backed_api.views import RESTAPIView,RESTAPIView2,RESTAPIView3
+from backed_api.views import *
 
 #urlpatterns = [path('admin/', admin.site.urls),path('api/v1/womenlist/', WomenAPIView.as_view())]
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/apis/', RESTAPIView.as_view())
+    #path('api/v1/apis/', include('backed_api.urls'))
+    path('api/getall/', RESTAPIView.as_view()),
+    path('api/parameters/', RESTAPIView2.as_view()),
+    path('api/data/', RESTAPIView3.as_view()),
 ]
--- src/agentAPI/backed_api/views.py
+++ src/agentAPI/backed_api/views.py
@@ -7,54 +7,45 @@ from rest_framework.views import APIView
 from .models import APIWEB
 from .main import one_task, get_web_from_name
 import connect_with_sql
+from django.http import HttpResponse
 from .serializer import APISerializer
 
 class RESTAPIView(APIView):
     def get(self, request):
-        if request.data == {}:
-            return Response({'title': 'GO OUT!!!'})  # для вызова из браузера
-        if request.data['request']=='get_all_api':
-            list_api_param = []
-            apis = connect_with_sql.get_API()
-            for i in range(len(apis)):
-                res = connect_with_sql.get_title(i + 1)
-                #print(res)
-                api = {'title': res[0][0],'api': apis[i], 'description': res[0][1]}
-                list_api_param.append(api)
-            return Response({'apis': list_api_param})
+        list_api_param = []
+        apis = connect_with_sql.get_API()
+        for i in range(len(apis)):
+            res = connect_with_sql.get_title(i + 1)
+            api = {'title': res[0][0],'api': apis[i], 'description': res[0][1]}
+            list_api_param.append(api)
+        return Response({'apis': list_api_param})
 
-        if request.data['request'] == 'get_some_parameters':
-            task_id = request.data['task_id']
-            user_id = request.data['user_id']
-            some_api = request.data['APIs']
-            list_api_param=[]
-            for i in range(len(some_api)):
-                param =connect_with_sql.get_param(i + 1)
-                title = connect_with_sql.get_title(i + 1)
-                api={'title': title[0][0],'api': some_api[i],'description': title[0][1]}
-                api={'api':api,'parameters':param}
-                #parameter={'parameters':param}
-                list_api_param.append(api)
-            return Response({'task_id':task_id,'user_id':user_id,'tasks':list_api_param})
 
-        if request.data['request']=='get_data':
-            list_res=[]
-            task_id = request.data['task_id']
-            user_id = request.data['user_id']
-            tasks = request.data['tasks']
-            for k in range(len(tasks)):
-                res=get_web_from_name(tasks[k])#получил ссылку на отдельный АПИ
-                request_to_anton=one_task(res)#это я отправляю Антону JSON с ссылкой и параметрами и получаю от него ответ
-                web_api=connect_with_sql.from_web_to_api(request_to_anton['web'])#заменяю в ответе Антона ссылку на название АПИ
-                request_to_user={'api': web_api,'data':[request_to_anton['parameters']]}#формирую ответ из названия АПИ и его данных
-                list_res.append(request_to_user)
-
-            return Response({'task_id':task_id, 'user_id': user_id,'task_data':list_res})
-
+class RESTAPIView2(APIView):
     def post(self, request):
-        post_new=APIWEB.objects.create(
-            title=request.data['title'],
-            content=request.data['content'],
-            cat_id=request.data['cat_id']
-        )
-        return Response({'post': model_to_dict(post_new)})
+        task_id = request.data['task_id']
+        user_id = request.data['user_id']
+        some_api = request.data['APIs']
+        list_api_param = []
+        for i in range(len(some_api)):
+            param = connect_with_sql.get_param(i + 1)
+            title = connect_with_sql.get_title(i + 1)
+            api = {'title': title[0][0], 'api': some_api[i], 'description': title[0][1]}
+            api = {'api': api, 'parameters': param}
+            # parameter={'parameters':param}
+            list_api_param.append(api)
+        return Response({'task_id': task_id, 'user_id': user_id, 'tasks': list_api_param})
+
+class RESTAPIView3(APIView):
+    def post(self, request):
+        list_res = []
+        task_id = request.data['task_id']
+        user_id = request.data['user_id']
+        tasks = request.data['tasks']
+        for k in range(len(tasks)):
+            res = get_web_from_name(tasks[k])  # получил ссылку на отдельный АПИ
+            request_to_anton = one_task(res)  # это я отправляю Антону JSON с ссылкой и параметрами и получаю от него ответ
+            web_api = connect_with_sql.from_web_to_api(request_to_anton['web'])  # заменяю в ответе Антона ссылку на название АПИ
+            request_to_user = {'api': web_api,'data': [request_to_anton['parameters']]}  # формирую ответ из названия АПИ и его данных
+            list_res.append(request_to_user)
+        return Response({'task_id': task_id, 'user_id': user_id, 'task_data': list_res})
